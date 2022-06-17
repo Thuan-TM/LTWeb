42d4cf4936d96fb2a952e3b5e8849ffc1edba508
TM <phamvanthuan0107@gmail.com>
2022-06-17 23:04:17 +0700

    .

--- animate/animate.js
+++ animate/animate.js
@@ -2,7 +2,10 @@ window.onload = ()=>{
     const animateds = document.querySelectorAll(".animated[type_animate]")
     let observer = new IntersectionObserver (entries => {
         entries.forEach(e => {
-            e.target.classList.toggle('active', e.isIntersecting)
+            e.target.classList.add('active')
+            if(e.boundingClientRect.top > e.rootBounds.height){
+                e.target.classList.remove('active')
+            }
         })
     },)
 
