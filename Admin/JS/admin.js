38e8525cfc663a65f7a59284cb071d53ef806153
TM <phamvanthuan0107@gmail.com>
2022-06-20 00:47:26 +0700

    Login voi Admin

--- Admin/JS/admin.js
+++ Admin/JS/admin.js
@@ -4,3 +4,17 @@ var listChucNang = document.querySelector('.list_chucNang');
 nav.onclick = () => {
   listChucNang.classList.toggle('open');
 }
+console.log(localStorage.Islogin)
+if(localStorage.Islogin === 'false') {
+  window.location = './../Form_Login/Login.html'
+}
+
+//  dang suat
+
+const logOut = document.querySelector('.Logout')
+logOut.onclick = ()=> {
+  localStorage.setItem('Islogin',false)
+  // window.location = './../Form_Login/Login.html'
+
+  // window.location = './../Form_Login/Login.html'
+}
