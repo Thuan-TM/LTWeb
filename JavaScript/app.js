e01e4e32987bc1a5dbc7287f49c9989eb7be3e6d
Dorayaky-Emon <95111610+Thuan-TM@users.noreply.github.com>
2022-06-22 11:11:27 +0700

    Update app.js

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,6 +1,6 @@
 const comHome = document.querySelector('.comehome')
 window.onscroll = ()=> {
-    if(window.scrollY > 0){
+    if(window.scrollY > 200){
         comHome.classList.remove("hide")
     }
     else {comHome.classList.add("hide")}
